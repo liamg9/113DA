29e57dae1a14ae42e877108ee17ad2db5d3192c4
Liam Greubel <liam.greubel@gmail.com>
2022-11-03 17:07:18 -0700

    nn finished

---
+++ nn_miniproject_base_/Core/Inc/jwread.h
@@ -0,0 +1,13 @@
+/*
+ * jwread.h
+ *
+ *  Created on: Nov 9, 2021
+ *      Author: charliezjw
+ */
+
+#ifndef LIBS_JWREAD_H_
+#define LIBS_JWREAD_H_
+
+float * ProcessBmp(char *textin);
+
+#endif /* LIBS_JWREAD_H_ */
